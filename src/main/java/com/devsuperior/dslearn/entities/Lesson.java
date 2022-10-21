1f224f9c1479d0a3686a4284c03f8b5c07d32bec
PradoMatheus <pradomatheus.19#hotmail.com>
2022-10-21 01:10:57 -0300

    Lesson, Content, Task

---
+++ src/main/java/com/devsuperior/dslearn/entities/Lesson.java
@@ -0,0 +1,63 @@
+package com.devsuperior.dslearn.entities;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "tb_lesson")
+@Inheritance(strategy = InheritanceType.JOINED)
+public abstract class Lesson implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private Integer position;
+    @ManyToOne
+    @JoinTable(name = "section_id")
+    private Section section;
+    @Setter(AccessLevel.NONE)
+    @ManyToMany
+    @JoinTable(name = "tb_lessons_done",
+            joinColumns = @JoinColumn(name = "lesson_id"),
+            inverseJoinColumns = {
+                    @JoinColumn(name = "user_id"),
+                    @JoinColumn(name = "offer_id")
+            }
+    )
+    private Set<Enrollment> enrollmentsDone = new HashSet<>();
+
+    public Lesson() {
+    }
+
+    public Lesson(Long id, String title, Integer position, Section section) {
+        this.id = id;
+        this.title = title;
+        this.position = position;
+        this.section = section;
+        this.enrollmentsDone = enrollmentsDone;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Lesson lesson = (Lesson) o;
+        return Objects.equals(id, lesson.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
